a229c34138a17a0a19f076d280a7f3bbd2751db8
StrayMarimo <anbasbas@gbox.adnu.edu.ph>
2023-12-05 14:09:06 +0800

    added trainor

--- fsl_predictor.py
+++ fsl_predictor.py
@@ -16,7 +16,12 @@ model.load_weights('action.h5')
 mp_holistic = mp.solutions.holistic # Holistic model
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
-actions = np.array(['a', 'b', 'c'])
+actions = np.array([
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u','v','w','x','y','z','0','1','2','3','4','5',
+    '6','7','8','9','10'
+])
 def draw_landmarks(image, results):
     
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
@@ -54,13 +59,13 @@ def extract_keypoints(results):
 
 
 colors = [(245,117,16), (117,245,16), (16,117,245)]
-def prob_viz(res, actions, input_frame, colors):
-    output_frame = input_frame.copy()
-    for num, prob in enumerate(res):
-        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
-        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+# def prob_viz(res, actions, input_frame, colors):
+#     output_frame = input_frame.copy()
+#     for num, prob in enumerate(res):
+#         cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+#         cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
         
-    return output_frame
+#     return output_frame
 
 
 # 1. New detection variables
@@ -93,25 +98,24 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
         if len(sequence) == 30:
             res = model.predict(np.expand_dims(sequence, axis=0))[0]
             print(actions[np.argmax(res)])
-            
-            
-            #3. Viz logic
-            if res[np.argmax(res)] > threshold: 
-                if len(sentence) > 0: 
-                    if actions[np.argmax(res)] != sentence[-1]:
-                        sentence.append(actions[np.argmax(res)])
-                else:
-                    sentence.append(actions[np.argmax(res)])
+             
+            # #3. Viz logic
+            # if res[np.argmax(res)] > threshold: 
+            #     if len(sentence) > 0: 
+            #         if actions[np.argmax(res)] != sentence[-1]:
+            #             sentence.append(actions[np.argmax(res)])
+            #     else:
+            #         sentence.append(actions[np.argmax(res)])
 
             if len(sentence) > 5: 
                 sentence = sentence[-5:]
 
             # Viz probabilities
-            image = prob_viz(res, actions, image, colors)
+            # image = prob_viz(res, actions, image, colors)
             
-        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
-        cv2.putText(image, ' '.join(sentence), (3,30), 
-        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        # cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        # cv2.putText(image, ' '.join(sentence), (3,30), 
+        # cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
         
         # Show to screen
         cv2.imshow('OpenCV Feed', image)
--- fsl_trainor.py
+++ fsl_trainor.py
@@ -12,7 +12,12 @@ import os
 DATA_PATH = 'MP_Data'
 # subfolder_names = ['a', 'b', 'c']
 # print(subfolder_names)
-actions = np.array(['a', 'b', 'c'])
+actions = np.array([
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u','v','w','x','y','z','0','1','2','3','4','5',
+    '6','7','8','9','10'
+])
 
 no_sequences = 30
 sequence_length = 30
